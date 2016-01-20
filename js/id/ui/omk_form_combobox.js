279a9e5011449bb0d6eda160108a713814dc5c6b
Rich Gwozdz <rgwozdz@spatialdev.com>
2016-01-20 15:32:49 -0800

    Insert a new dropdown menu in UI.

---
+++ js/id/ui/omk_form_combobox.js
@@ -0,0 +1,34 @@
+iD.ui.OmkFormCombobox = function(context) {
+
+    return function omkFormCombobox(selection){
+
+        var options = function(type) {
+            var options = ['choice-1', 'choice-2', 'choice-3'];
+
+            return options.map(function(option) {
+                return {
+                    title: option,
+                    value: option
+                };
+            });
+        };
+
+        selection
+            .append('input')
+            .attr('type', 'text')
+            .attr('class', 'omk-form')
+            .attr('id', function(d) {
+                return 'omk-form-item';
+            })
+            .each(function(d) {
+                d3.select(this)
+                    .call(d3.combobox()
+                        .data(options(d)));
+            });
+
+
+    }
+    var dispatch = d3.dispatch('change');
+
+    return d3.rebind(access, dispatch, 'on');
+};
