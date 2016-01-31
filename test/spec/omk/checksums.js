d13a39a455434601940a2b299630818561c8da75
Nicholas Hallahan <nick@theoutpost.io>
2016-01-31 14:00:36 -0800

    Able to isolate OSM entities in unit test

--- test/spec/omk/checksums.js
+++ test/spec/omk/checksums.js
@@ -1,7 +1,16 @@
 describe('OMK.Checksums', function () {
 
-    it('test should fail', function () {
-        expect('abc').to.eql('aaa');
+    it('rusha.js should hash to the same test string as OMK Android', function () {
+        var r = new Rusha();
+        var sha1 = r.digest('test');
+        expect(sha1).to.eql('a94a8fe5ccb19ba61c4c0873d391e987982fbbd3');
     });
+
+    it('should be able to fetch OSM XML and create an array of Entities', function () {
+        OMK.fetchXmlAndCreateEntities('data/checksum_way.xml', function (entities) {
+            expect(entities.length).to.eql(6);
+        });
+    });
+
 
 });
